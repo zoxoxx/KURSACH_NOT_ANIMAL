0618ab3ac359554f7f5a6ea3d5939c0bf2524a84
zoxoxx <spaorto740@gmail.com>
2025-05-05 20:00:29 +0300

    Subsystem "Sklad purchases" added

--- KURSACH_NOT_ANIMAL/Forms/Admin/Sklad/SkladReestr.cs
+++ KURSACH_NOT_ANIMAL/Forms/Admin/Sklad/SkladReestr.cs
@@ -1,4 +1,5 @@
 ﻿using KURSACH_NOT_ANIMAL.Classes.ViewClasses;
+using KURSACH_NOT_ANIMAL.Model;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,20 +14,68 @@ namespace KURSACH_NOT_ANIMAL.Forms.Admin.Sklad
 {
     public partial class SkladReestr : Form
     {
-        private List<SkladView> purchases;
+        private List<SkladView>? purchases;
         public SkladReestr()
         {
             InitializeComponent();
+
+            DG_SKLAD.Columns[0].DataPropertyName = "Id";
+            DG_SKLAD.Columns[1].DataPropertyName = "ProductName";
+            DG_SKLAD.Columns[2].DataPropertyName = "Count";
+            DG_SKLAD.Columns[3].DataPropertyName = "ShopName";
+            DG_SKLAD.Columns[4].DataPropertyName = "PartnerName";
+            DG_SKLAD.Columns[5].DataPropertyName = "PurchasePrice";
+            DG_SKLAD.Columns[6].DataPropertyName = "DatePrihod";
+
+            DG_SKLAD.AutoGenerateColumns = false;
         }
 
         private void SkladReestr_Load(object sender, EventArgs e)
         {
-
+            DataGridLoad();
         }
 
         private void DataGridLoad()
         {
-            //DG_SK
+            purchases = SkladFromDb.GetAllPurchases();
+            DG_SKLAD.DataSource = purchases;
+        }
+
+        private void BTN_ADD_Click(object sender, EventArgs e)
+        {
+            SkladForm skladForm = new SkladForm(flagInsert: true);
+            this.Hide();
+            skladForm.ShowDialog();
+            this.Show();
+
+            DataGridLoad();
+        }
+
+        private void BTN_DELETE_Click(object sender, EventArgs e)
+        {
+            SkladView? selectedPurchase = DG_SKLAD.CurrentRow.DataBoundItem as SkladView;
+
+            if (selectedPurchase is null)
+                return;
+
+            SkladFromDb.DeletePurchase(selectedPurchase.Id);
+
+            DataGridLoad();
+        }
+
+        private void DG_SKLAD_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            SkladView? selectedPurchase = DG_SKLAD.CurrentRow.DataBoundItem as SkladView;
+
+            if (selectedPurchase is null)
+                return;
+
+            SkladForm skladForm = new SkladForm(flagUpdate: true, purchase: selectedPurchase);
+            this.Hide();
+            skladForm.ShowDialog();
+            this.Show();
+
+            DataGridLoad();
         }
     }
 }
---
+++ KURSACH_NOT_ANIMAL/Model/ProductFromDb.cs
@@ -0,0 +1,51 @@
+﻿using KURSACH_NOT_ANIMAL.Classes.DbClasses;
+using KURSACH_NOT_ANIMAL.Models;
+using Microsoft.VisualBasic.Logging;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KURSACH_NOT_ANIMAL.Model
+{
+    public class ProductFromDb
+    {
+        public static List<Product>? GetProducts()
+        {
+            List<Product> products = new List<Product>();
+
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection(ConnectionStr.connectionString))
+                {
+                    connection.Open();
+
+                    string sqlExp = "select ID, NAME " +
+                        "from PRODUCT";
+                    NpgsqlCommand cmd = new NpgsqlCommand(sqlExp, connection);
+
+                    NpgsqlDataReader reader = cmd.ExecuteReader();
+
+                    if (!reader.HasRows)
+                        return null;
+
+                    while (reader.Read())
+                        products.Add(new Product((int)reader[0], reader[1].ToString()!));
+                }
+            }
+            catch(NpgsqlException ex)
+            {
+                Debug.WriteLine(ex.Message);
+                MessageBox.Show("Было вызвано исключение при проверке наличия пользователя в системе,\n" +
+                    "уведомьте разработчиков.", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                return null;
+            }
+
+            return products;
+        }
+    }
+}
