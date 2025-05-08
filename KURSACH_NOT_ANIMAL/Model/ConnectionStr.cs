d914f5f730d0e67a012d37322045ec78ad508843
ubrioniks <ivan_semin_04@mail.ru>
2025-05-09 01:19:50 +0300

    Minor fixes

--- KURSACH_NOT_ANIMAL/Model/ConnectionStr.cs
+++ KURSACH_NOT_ANIMAL/Model/ConnectionStr.cs
@@ -8,6 +8,6 @@ namespace KURSACH_NOT_ANIMAL.Models
 {
     public class ConnectionStr
     {
-        public static string connectionString = @"Server=localhost; Port=5432 ; DataBase=KursachNotAnimal; User Id=postgres;Password=123";
+        public static string connectionString = @"Server=localhost; Port=5432 ; DataBase=KursachNotAnimal; User Id=postgres;Password=971254";
     }
 }
